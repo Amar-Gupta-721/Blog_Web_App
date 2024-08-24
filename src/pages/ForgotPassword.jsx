184947c66c618fd9332fcaecbf19be6b54970422
Amar-Gupta-721 <amargupta721@gmail.com>
2024-08-24 22:34:52 +0530

    update forgotPassword

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -21,7 +21,7 @@ const ForgotPassword = () => {
     };
 
     return (
-        <div className='flex items-center justify-center w-full'>
+        <div className='flex items-center justify-center w-full pt-4'>
             <div className="mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10">
                 <h2 className="text-center text-2xl font-bold leading-tight">Forgot Password</h2>
                 <p className="mt-2 text-center text-base text-black/60">
