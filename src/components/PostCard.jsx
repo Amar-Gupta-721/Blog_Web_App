64f7f17c0e282c2d7793bb13c7c003c23b878065
Amar-Gupta-721 <amargupta721@gmail.com>
2025-04-29 15:28:53 +0530

    update home

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -19,55 +19,48 @@ function PostCard({ post }) {
     };
 
     const formattedDate = date.toLocaleDateString("en-US", options);
-
     setDate(formattedDate);
-  }, []);
+  }, [post.$createdAt]);
+
   return (
-    <>
-      <div className="my-4 mx-2 backdrop-blur-3xl lg:m-8 md:m-4 sm:m-2 px-30 hover:shadow-md duration-200 ease-in-out hover:scale-105">
-        <div className="flex shadow-xl overflow-auto rounded-xl max-h-48 lg:max-h-64 lg:min-h-56 ">
-          <div className="w-2/5 mr-2">
-            <img
-              className="w-full h-full max-w-48 max-h-48 lg:max-h-80 lg:max-w-96 md:max-w-80 sm:max-w-52 sm:max-h-52 rounded-l-xl"
-              src={appwriteService.getFilePreview(post.featuredImage)}
-              alt={post.title}
+    <div
+      className="group bg-white/10 backdrop-blur-md shadow-md hover:shadow-2xl transition-all duration-300 rounded-2xl overflow-hidden 
+                    w-full max-w-[400px] h-[400px] flex flex-col"
+    >
+      <div className="h-[50%] w-full overflow-hidden">
+        <img
+          className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
+          src={appwriteService.getFilePreview(post.featuredImage)}
+          alt={post.title}
+        />
+      </div>
+
+      <div className="h-[50%] w-full flex flex-col justify-between p-4">
+        <div className="flex items-center text-neutral-200 text-xs md:text-sm">
+          <AccessTimeIcon className="mr-2" fontSize="small" />
+          <span>{date}</span>
+        </div>
+
+        <Link to={`/post/${post.$id}`}>
+          <h2 className="text-white font-semibold text-lg md:text-xl hover:text-blue-400 transition-colors duration-300 truncate capitalize mt-2">
+            {post.title}
+          </h2>
+        </Link>
+
+        <p className="text-neutral-300 text-xs md:text-sm mt-2">
+          By: <span className="font-medium">{post.userName}</span>
+        </p>
+
+        <div className="mt-3">
+          <Link to={`/post/${post.$id}`}>
+            <Button
+              children={"Read More"}
+              className="!text-black bg-white rounded-full px-5 py-2 text-xs md:text-sm hover:bg-neutral-300 hover:text-black transition-all duration-300"
             />
-          </div>
-          <div className="w-3/5 text-left xl:px-4 lg:px-4 md:px-2 py-1 xl:py-3 lg:py-3 md:py-3 bg-inherit">
-            <div className="flex items-center text-neutral-50 text-sm lg:text-xl md:text-lg sm:text-sm">
-              <AccessTimeIcon
-                sx={{
-                  fontSize: {
-                    xs: "16px",
-                    sm: "20px",
-                    md: "24px",
-                    lg: "28px",
-                    xl: "28px",
-                  },
-                  marginRight: "0.25rem",
-                }}
-              />
-              <span>{date}</span>
-            </div>
-            <Link to={`/post/${post.$id}`}>
-              <h2 className="py-2 font-semibold text-neutral-50 capitalize truncate text-md lg:text-4xl md:text-3xl sm:text-2xl">
-                {post.title}
-              </h2>
-            </Link>
-            <h4 className="text-neutral-50 text-sm lg:text-xl md:text-lg sm:text-sm">
-              By : {post?.userName || "User"}
-            </h4>{" "}
-            {/*change (post?.userName || "user") by post.userName */}
-            <Link to={`/post/${post.$id}`}>
-              <Button
-                children={"Read More"}
-                className="!text-neutral-950 bg-white text-xs md:text-sm lg:text-base xl:text-lg mt-2 xl:mt-3 lg:mt-3 md:mt-3 hover:bg-neutral-300 hover:text-black transition-all duration-200"
-              />
-            </Link>
-          </div>
+          </Link>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,85 +1,92 @@
-import React, {useState, useEffect} from 'react'
-import appwriteService from '../appwrite/config'
-import { Container, PostCard } from '../components'
-import { useSelector } from 'react-redux'       
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import appwriteService from "../appwrite/config";
+import { Container, PostCard } from "../components";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 function Home() {
-    const userData = useSelector(state=>state.auth.userData)    
-    const [posts, setPosts] = useState([])
-    const [loading, setLoading] = useState(true)
+  const userData = useSelector((state) => state.auth.userData);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        if (userData) {       
-            appwriteService.getPosts().then((posts) => {
-                if (posts) {
-                    const active = posts.documents.filter(post => post.status=='active');
-                    setPosts(active)
-                }
-                setLoading(false) 
-            }).catch(() => {
-                setLoading(false) 
-            })
-        }
-        else{
-            setLoading(false)
-        }
-    }, [userData])//
-
-    if (loading) {
-        return (
-            <div className="w-full py-8 mt-4 text-center">
-                <Container>
-                    <h1 className="text-2xl font-bold">Loading...</h1>
-                </Container>
-            </div>
-        )
+  useEffect(() => {
+    if (userData) {
+      appwriteService
+        .getPosts()
+        .then((posts) => {
+          if (posts) {
+            const activePosts = posts.documents.filter(
+              (post) => post.status === "active"
+            );
+            setPosts(activePosts);
+          }
+          setLoading(false);
+        })
+        .catch(() => {
+          setLoading(false);
+        });
+    } else {
+      setLoading(false);
     }
+  }, [userData]);
 
-  if(userData==null)return (
-    <div className="w-full py-8 mt-4 text-center">
-        <Container>
-            <Link to="/login">
-                <h1 className="text-2xl font-bold hover:text-gray-500">
-                    Login to read posts
-                </h1>
-            </Link>
-        </Container>
-    </div>
-  )
-
-  if(posts.length === 0){
+  if (loading) {
     return (
-        <div className="w-full py-8 mt-4 text-center">
-            <Container>
-                <div className="flex flex-wrap">
-                    <div className="p-2 w-full">
-                        <h1 className="text-2xl font-bold">
-                            No post has been created Yet...
-                        </h1>
-                        <br />
-                        <Link to='/add-post'>
-                            <span className='text-4xl font-bold underline text-neutral-600 hover:text-neutral-950 '>Add some Post..</span>
-                        </Link>
-                    </div>
-                </div>
-            </Container>
-        </div>
-    )
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <h1 className="text-2xl font-bold">Loading...</h1>
+        </Container>
+      </div>
+    );
   }
+
+  if (!userData) {
+    return (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <Link to="/login">
+            <h1 className="text-2xl font-bold hover:text-gray-500">
+              Login to read posts
+            </h1>
+          </Link>
+        </Container>
+      </div>
+    );
+  }
+
+  if (posts.length === 0) {
+    return (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <div className="flex flex-wrap">
+            <div className="p-2 w-full">
+              <h1 className="text-2xl font-bold">
+                No posts have been created yet...
+              </h1>
+              <br />
+              <Link to="/add-post">
+                <span className="text-4xl font-bold underline text-neutral-600 hover:text-neutral-950">
+                  Add some posts..
+                </span>
+              </Link>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+
   return (
     <div className="w-full py-8">
-        <Container>
-            <div className="flex flex-wrap">
-                {posts.map((post)=>(
-                    <div key={post.$id} className="p-2 w-full">
-                        <PostCard post={post}/>
-                    </div>
-                ))}
-            </div>
-        </Container>
+      <Container>
+        <div className="flex flex-wrap gap-6 justify-start">
+          {posts.map((post) => (
+            <PostCard post={post} />
+          ))}
+        </div>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
